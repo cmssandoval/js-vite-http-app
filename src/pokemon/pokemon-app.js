60c90d2ab44089cb7c6f09a4e3fe7342b3e6413e
Carlos Macías Sandoval <carlossan.16.mc@gmail.com>
2026-08-16 18:17:15 -0400

    feat(PokemonApp): add HTML elements to display pokemons data
    > Created pokemonIdState.
    > Added paragraph, image, and next/previous buttons.
    > Added loading text for paragraph.
    > Created renderPokemon function to fill the HTML elements with the corresponding data based on id.
    > Added event listeners to the buttons to call the getPokemonById function iterating the id based on pokemonIdState.

--- src/pokemon/pokemon-app.js
+++ src/pokemon/pokemon-app.js
@@ -4,11 +4,50 @@ import { getPokemonById } from './actions/get-pokemon-by-id.action';
  * Esta función crea la aplicación en el div enviado.
  * @param {HTMLDivElement} element Elemento div HTML.
  */
-export const PokemonApp = ( element ) => {
+export const PokemonApp = async ( element ) => {
+
+    let pokemonIdState = 1;
 
     document.title = 'Pokemon App';
     const appTitle = document.querySelector('#app-title');
     if ( appTitle ) appTitle.innerHTML = 'Pokemon App';
     
-    getPokemonById( 1 );
+    //* Crear elementos HTML
+    const loadingParagraph = document.createElement('p');
+    const pokemonImage = document.createElement('img');
+    const nextButton = document.createElement('button');
+    const previousButton = document.createElement('button');
+
+    //* Asignar valores a los elementos
+    loadingParagraph.textContent = 'Cargando...';
+    nextButton.textContent = 'Siguiente';
+    previousButton.textContent = 'Anterior';
+
+    //* Insertar elementos creados al div
+    element.appendChild(loadingParagraph);
+    element.appendChild(pokemonImage);
+    element.appendChild(nextButton);
+    element.appendChild(previousButton);
+
+    //* Listeners de los botones
+    nextButton.addEventListener('click', () => {
+        getPokemonById( pokemonIdState + 1 )
+            .then( renderPokemon, pokemonIdState++ );
+    });
+
+    previousButton.addEventListener('click', () => {
+        if ( pokemonIdState < 2 ) return;
+        getPokemonById( pokemonIdState - 1 )
+            .then( renderPokemon, pokemonIdState-- );
+    });
+    
+    //* Renderizar el pokemon
+    const renderPokemon = ( pokemon ) => {
+        pokemonImage.src = pokemon.image;
+        loadingParagraph.textContent = `
+            Pokemon #${ pokemon.id } - ${ pokemon.name }`;
+    }
+
+    //* Hacer la petición inicial
+    renderPokemon( await getPokemonById( pokemonIdState ) );
 };
